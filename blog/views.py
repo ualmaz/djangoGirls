82ae46c65682d368c8c9dfc9201a0b42a7834527
ualmaz <ualmaz@gmail.com>
2018-09-23 21:07:55 +0600

    modified

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Record
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    records = Record.objects.all()
+    return render(request, 'blog/post_list.html', {'records': records})
