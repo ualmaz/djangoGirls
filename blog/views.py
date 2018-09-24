fbd2208411e8c83585618c6c1fb2479fc9bf3284
ualmaz <ualmaz@gmail.com>
2018-09-24 09:27:43 +0600

    modified

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Record
 
 def post_list(request):
     records = Record.objects.all()
     return render(request, 'blog/post_list.html', {'records': records})
+
+def post_detail(request, pk):
+    record = get_object_or_404(Record, pk=pk)
+    return render(request, 'blog/post_detail.html', {'record': record})
